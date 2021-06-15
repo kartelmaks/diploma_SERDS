c80ecc2283f4d7d7c692afd9ce66561543f8c66d
kartelmaksym <kartellmaks@gmail.com>
2021-06-15 16:00:34 +0300

    change Plot1 test commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ X1 = np.array(pd.read_csv('V784.csv', header=None)[1])
 X2 = np.array(pd.read_csv('V784_5.csv', header=None)[1])
 X3 = np.array(pd.read_csv('V785.csv', header=None)[1])
 X_axes = pd.read_csv('V784.csv', header=None)[0]
-
+# ssvsd
 serds = SERDS([X1, X2, X3], [0, -6, -12])
 
 raman, fluor = serds.get_reconstr_spectrs()
