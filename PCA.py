900e4e5bd900d4b2d38446d4aebfacf984df3bc5
kartelmaksym <kartellmaks@gmail.com>
2021-06-20 19:59:09 +0300

    pca2

--- PCA.py
+++ PCA.py
@@ -1,17 +1,8 @@
 import numpy as np
 import pandas as pd
 import scipy as sp
-import matplotlib
 import matplotlib.pyplot as plt
-import plotly.express as px
-
-from scipy.signal import savgol_filter
-from plotly.graph_objs import *
-import plotly.offline as offline
-
-from sklearn.decomposition import PCA as PCA
 from scipy.linalg import svd
-
 plt.style.use('ggplot')
 
 class PCA:
@@ -22,5 +13,29 @@ class PCA:
 
         T = U.dot(S)[:, :n_com]
         P = Vt.T[:, :n_com]
+        return T, P
 
+    def image(self, n_com, data, P, X, x_axes, labels):
+        #n_com = 6
+        #data = fluor_df
+        #U, s, Vt = svd(data, full_matrices=0)
+        #S = np.diag(s)
+        #P = Vt.T[:, :n_com]
+        #X_all = [X_df]
+        #labels = ['fluor_df']
+
+        fig_, axes_ = plt.subplots(nrows=2, ncols=n_com, figsize=(100, 20))
+        y1 = x_axes.T[0]
+        for r in range(P.shape[1]):
+            axes_[0, r].set(title="ГК " + str(r + 1))
+            axes_[0, r].plot(y1, P.T[r, :])
+        k = 0
+        for key in labels:
+            Q = np.linalg.lstsq(P, X[k].T)[0].T
+
+            for i in range(n_com):
+                Q_ = Q[:, i].reshape(58, 141)
+                axes_[k + 1, i].imshow(Q_)
+                axes_[k + 1, 0].set_ylabel(key)
+            k += 1
 
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
+
 from SERDS import SERDS
 from Plot import Plot
+from PCA import PCA
 
 X1 = np.array(pd.read_csv('V784.csv', header=None)[1])
 X2 = np.array(pd.read_csv('V784_5.csv', header=None)[1])
@@ -15,3 +17,6 @@ raman, fluor = serds.get_reconstr_spectrs()
 Plot.plot_all([X1, X2, X3], raman, fluor, X_axes, legend = ['784 нм', '785 нм', '786 нм'])
 
 Plot.plot_spectrs([X1, X2, X3], X_axes)
+
+T, P = PCA.pca(6,[X1, X2, X3])
+PCA.image(6,raman, [X1, X2, X3], P, X_axes, ['raman'])
